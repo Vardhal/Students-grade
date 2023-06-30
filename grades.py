5a69aa50ac6fbdebd491ab074062d0f4369287a3
Bigby <monrxwlob@gmail.com>
2023-06-30 19:21:23 +0300

    class Course added

--- grades.py
+++ grades.py
@@ -7,4 +7,24 @@ class Student:
     def get_grade(self):
         return self.grade    
     
+class Course:
+    
+    def __init__(self, c_title, max_students):
+        self.c_title = c_title
+        self.max_students = max_students
+        self.students = []
+        
+    def add_student(self, student):
+        if len(student) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+        
+    def average_grade(self):
+        value = 0
+        for student in self.students:
+            value += student.get_grade()
+            
+        return value / len(self.students)            
+    
     

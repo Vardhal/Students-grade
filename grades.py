7f8025a3ec299a91828a2a53965f9d70e7d674f9
Bigby <monrxwlob@gmail.com>
2023-06-30 19:09:31 +0300

    Student class added

---
+++ grades.py
@@ -0,0 +1,10 @@
+class Student:
+    
+    def __init__(self, name, grade):
+        self.name = name
+        self.grade = grade
+        
+    def get_grade(self):
+        return self.grade    
+    
+    
